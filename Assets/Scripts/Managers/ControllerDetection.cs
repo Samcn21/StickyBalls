acaddb85be597fb92c12a95d4414d685b1bd05b3
Sam <sayid.malekdoost@gmail.com>
2016-04-15 18:29:04 +0200

    Controller assign

---
+++ Assets/Scripts/Managers/ControllerDetection.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using GamepadInput;
+
+public class ControllerDetection : MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GamePad.GetButtonDown(GamePad.Button.Start, GamePad.Index.One))
+        {
+            Debug.Log("Num1");
+        }
+        else if (GamePad.GetButtonDown(GamePad.Button.Start, GamePad.Index.Two))
+        {
+            Debug.Log("Num2");
+            
+        }
+        
+	}
+}
