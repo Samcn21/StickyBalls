db66693ae9380fe0aa7501c5c97a86989cab7fe9
Sam <sayid.malekdoost@gmail.com>
2016-04-19 17:38:03 +0200

    Animation Setup

--- Assets/Scripts/Temp/PlayerMovement.cs
+++ Assets/Scripts/Temp/PlayerMovement.cs
@@ -6,20 +6,21 @@ public class PlayerMovement : MonoBehaviour {
     private Rigidbody rigidbody;
     private GamepadState gamepadState;
     //private GamePad.Index gamepadIndex;
-    private Player player;
+    //private Player player;
 
     [SerializeField]
     private float stickSensivity = 0.25f;
 
 	// Use this for initialization
 	void Start () {
-        //gamepadState = GamePad.GetState(GamePad.Index.);
+        gamepadState = GamePad.GetState(GamePad.Index.Any);
         player = GetComponent<Player>();
         rigidbody = GetComponent<Rigidbody>();
 
 	}
     void FixedUpdate()
     {
-        //rigidbody.AddForce(new Vector3(gamepadState.LeftStickAxis.x * stickSensivity, 0, gamepadState.LeftStickAxis.y * stickSensivity) * player.moveSpeed);
+
+        rigidbody.AddForce(new Vector3(gamepadState.LeftStickAxis.x * stickSensivity, 0, gamepadState.LeftStickAxis.y * stickSensivity) * moveSpeed);
     }
 }
