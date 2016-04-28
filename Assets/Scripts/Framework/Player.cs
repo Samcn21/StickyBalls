efeed073ca99d4e819ad7366dbcde457659f6c4c
Martin <me_thomsen@hotmail.com>
2016-04-28 11:48:46 +0200

    pipe holding

--- Assets/Scripts/Framework/Player.cs
+++ Assets/Scripts/Framework/Player.cs
@@ -4,7 +4,38 @@ using System.Collections;
 public class Player : MonoBehaviour
 {
     [SerializeField] public float moveSpeed = 1f;
-    public PipeData.PipeType HeldPipeType { get; private set; }
+    [SerializeField] private Transform heldPipe;
+    [SerializeField] private Transform heldPipeContainer;
+
+    private MeshRenderer heldPipeRenderer;
+    private PipeData.PipeType heldPipeType;
+    private PipeMan pipeMan;
+
+    void Start()
+    {
+        pipeMan = GameController.Instance.PipeMan;
+        heldPipeRenderer = heldPipe.GetComponent<MeshRenderer>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+    public PipeData.PipeType HeldPipeType
+    {
+        get
+        {
+            return heldPipeType;
+        }
+        private set
+        {
+            heldPipeRenderer.enabled = (value != PipeData.PipeType.Void);
+            heldPipeRenderer.material = pipeMan.pipeTextures[value];
+            heldPipeType = value;
+        }
+    }
     
     public void PickupPipe(ConveyorPipe conPipe)
     {
@@ -15,5 +46,10 @@ public class Player : MonoBehaviour
     public void PlacePipe()
     {
         HeldPipeType = PipeData.PipeType.Void;
+    }
+
+    public void RotatePipe(int angle)
+    {
+        heldPipe.rotation = Quaternion.Euler(90, angle, 0);
     }
 }
