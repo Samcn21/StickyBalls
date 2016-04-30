15775334db5e11f248c8675c94572fb38228d8e1
Luca Salfi <lucasalfi2@gmail.com>
2016-04-30 12:16:08 +0200

    change of explosion prefab

---
+++ Assets/DynamiteLogic.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DynamiteLogic : MonoBehaviour {
+    private HashSet<GameObject> pipeToPhysicallyAffect;
+
+    void Awake()
+    {
+        pipeToPhysicallyAffect = new HashSet<GameObject>();
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        if (gameObject.tag == "Pipe")
+        {
+            pipeToPhysicallyAffect.Add(col.gameObject);
+            Debug.Log("I detected the pipe " + col.gameObject.name);
+        }
+    }
+}
