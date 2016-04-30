33c0674612a91c208a9ec634b4d0d747f3e6db3d
Sam <sayid.malekdoost@gmail.com>
2016-04-30 13:06:20 +0200

    Game controller

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -2,14 +2,20 @@
 using System.Collections;
 
 public class AudioManager : MonoBehaviour {
-
+    public AudioClip Music;
+    public AudioClip pickupPipeSFX;
+    public AudioClip placePipeSFX;
+    public AudioClip bombExplosion;
+    public AudioClip winSound;
+    public AudioClip rotatePipe;
+   
 	// Use this for initialization
 	void Start () {
-	
+        //GameObject.FindObjectOfType<AudioSource>().;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        
 	}
 }
