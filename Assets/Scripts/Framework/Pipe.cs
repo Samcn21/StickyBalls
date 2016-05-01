131b39984732283fc10c09d0b938a98c83ca8b12
Luca Salfi <lucasalfi2@gmail.com>
2016-05-01 19:11:42 +0200

    Changes to be committed: 	new file:   Assets/Prefabs/ParticleSystems.meta 	new file:   Assets/Prefabs/ParticleSystems/WeldParticleSystem.prefab 	new file:   Assets/Prefabs/ParticleSystems/WeldParticleSystem.prefab.meta 	modified:   Assets/Prefabs/Pipe.prefab 	new file:   Assets/Scenes/Main/Level03_MartinTestGround.unity 	new file:   Assets/Scenes/Main/Level03_MartinTestGround.unity.meta 	modified:   Assets/Scenes/Test/test_Luca.unity 	modified:   Assets/Scripts/Framework/Pipe.cs 	new file:   Assets/Scripts/Systems.meta 	new file:   Assets/Scripts/Systems/WeldsparksParticleSystem.cs 	new file:   Assets/Scripts/Systems/WeldsparksParticleSystem.cs.meta 	new file:   Assets/Sprites/Particles.meta 	new file:   Assets/Sprites/Particles/WeldSparks.meta 	new file:   Assets/Sprites/Particles/WeldSparks/Fireworks_Flare_D.png 	new file:   Assets/Sprites/Particles/WeldSparks/Fireworks_Flare_D.png.meta 	new file:   Assets/Sprites/Particles/WeldSparks/Fireworks_Tail_D.png 	new file:   Assets/Sprites/Particles/WeldSparks/Fireworks_Tail_D.png.meta 	new file:   Assets/Sprites/Particles/WeldSparks/Flare_Sparks_TexS.png 	new file:   Assets/Sprites/Particles/WeldSparks/Flare_Sparks_TexS.png.meta 	new file:   Assets/Sprites/Pipes.meta 	new file:   Assets/Sprites/Pipes/spirPipeStraightCyan1.png 	new file:   Assets/Sprites/Pipes/spirPipeStraightCyan1.png.meta

--- Assets/Scripts/Framework/Pipe.cs
+++ Assets/Scripts/Framework/Pipe.cs
@@ -5,6 +5,8 @@ using System.Linq;
 
 public class Pipe : MonoBehaviour
 {
+    [SerializeField] private GameObject WeldSparkParticleSystemPrefab;
+
     private PipeData.PipeType pipeType;
     public PipeData.PipeType PipeType
     {
@@ -67,13 +69,24 @@ public class Pipe : MonoBehaviour
         foreach (Vector2 v in pipeMan.pipeConnections[pipeType]) {
             Vector2 rotatedVector = Quaternion.Euler(0, 0, -rotationAngle) * v;
             GameData.Coordinate conCoord = new GameData.Coordinate(positionCoordinate.x + Mathf.RoundToInt(rotatedVector.x), positionCoordinate.y + Mathf.RoundToInt(rotatedVector.y));
-            if (conCoord.x >= 0 && conCoord.x <= gridController.Grid.GetLength(0) - 1 && conCoord.y >= 0 &&
-                conCoord.y <= gridController.Grid.GetLength(1) - 1) {
+            if (conCoord.x >= 0 && conCoord.x <= gridController.Grid.GetLength(0) - 1 && conCoord.y >= 0 && conCoord.y <= gridController.Grid.GetLength(1) - 1) {
                 connections.Add(conCoord);
                 if (gridController.Grid[conCoord.x, conCoord.y].pipe != null)
                 {
                     if (!connectedTeams.Contains(gridController.Grid[conCoord.x, conCoord.y].pipe.team))
                         connectedTeams.Add(gridController.Grid[conCoord.x, conCoord.y].pipe.team);
+                    float x = 0.5f;
+                    if (gridController.Grid[conCoord.x, conCoord.y].pipe.isSource)
+                        x = 0.25f;
+                    Vector3 sparkPos = Vector3.Lerp(transform.position, gridController.Grid[conCoord.x, conCoord.y].pipe.transform.position, x);
+                    GameObject sparks = Instantiate(WeldSparkParticleSystemPrefab, new Vector3(sparkPos.x, 0.5f, sparkPos.z) , Quaternion.Euler(-48.3f, 152.4f, 0)) as GameObject;
+                    WeldsparksParticleSystem sparksParticleSystem = sparks.GetComponent<WeldsparksParticleSystem>();
+                    bool isHorizontalWeld = false;
+                    if (rotatedVector.x > 0.5f || rotatedVector.x < -0.5f)
+                        isHorizontalWeld = true;
+                    else if (rotatedVector.y > 0.5f || rotatedVector.y < -0.5f)
+                        isHorizontalWeld = false;
+                    sparksParticleSystem.Initialize(isHorizontalWeld);
                 }
             }
         }
@@ -98,6 +111,7 @@ public class Pipe : MonoBehaviour
             }
         }
 
+        
     }
 
     public void DestroyPipe() {
---
+++ Assets/Scripts/Systems/WeldsparksParticleSystem.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeldsparksParticleSystem : MonoBehaviour
+{
+
+    [SerializeField] private float timeToLive = 0.7f;
+    [SerializeField] private float positionOffset = 0.3f;
+
+    private float currentTime = 0;
+    private Vector3 fromPos;
+    private Vector3 toPos;
+    private bool isInitialized = false;
+
+    public void Initialize(bool isHorizontal)
+    {
+        if (!isHorizontal)
+        {
+            fromPos = new Vector3(transform.position.x - positionOffset, transform.position.y, transform.position.z);
+            toPos = new Vector3(transform.position.x + positionOffset, transform.position.y, transform.position.z);
+        }
+        else
+        {
+            fromPos = new Vector3(transform.position.x, transform.position.y, transform.position.z + positionOffset);
+            toPos = new Vector3(transform.position.x, transform.position.y, transform.position.z - positionOffset);
+        }
+        isInitialized = true;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        if (!isInitialized) return;
+	    currentTime += Time.deltaTime;
+        if (currentTime > timeToLive)
+            Destroy(gameObject);
+
+	    transform.position = Vector3.Lerp(fromPos, toPos, currentTime/timeToLive);
+	}
+}
