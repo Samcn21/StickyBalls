14a884111c2d67a21db98a0fde30df94bd975a00
Sam <sayid.malekdoost@gmail.com>
2016-05-14 22:55:39 +0200

    play2vs2 pause init

--- Assets/Scripts/StateMachine/States/Play2vs2.cs
+++ Assets/Scripts/StateMachine/States/Play2vs2.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 using Assets.Scripts.Interfaces;
+using GamepadInput;
+
 namespace Assets.Scripts.States
 {
     public class Play2vs2 : IStateBase
@@ -11,13 +13,15 @@ namespace Assets.Scripts.States
             StateManager = managerRef;
             StateManager.CurrentActiveState = GameData.GameStates.Play2vs2;
         }
+
+
         public void StateUpdate()
         {
-            //if (isWinning))
-            //{
-            //    StateManager.SwitchState(new Winning2vs2(StateManager));
-            //      
-            //}
+            if (GamePad.GetButtonDown(GamePad.Button.Start, GamePad.Index.Any) || Input.GetKeyDown(KeyCode.Escape))
+            {
+                StateManager.PreActiveState = GameData.GameStates.Play2vs2;
+                StateManager.SwitchState(new Pause(StateManager));
+            }
         }
 
         public void StateFixedUpdate()
