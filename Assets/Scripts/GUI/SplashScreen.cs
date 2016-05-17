ec8201995c67bc5fa8f1d18e8d8e0432104041d1
Sam <sayid.malekdoost@gmail.com>
2016-05-17 18:52:07 +0200

    SplashScreens

---
+++ Assets/Scripts/GUI/SplashScreen.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    public string levelName = "MainMenu";
+    public float waitForSec = 3f;
+	
+	void Update () 
+    {
+        waitForSec -= Time.deltaTime;
+
+        if (waitForSec < 0)
+            SceneManager.LoadScene(levelName);
+	}
+}
