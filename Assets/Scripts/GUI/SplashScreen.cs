3d8c8e9697b66fb84e7087bf724511d7667bc975
Sam <sayid.malekdoost@gmail.com>
2016-05-17 20:31:51 +0200

    timer set and controller A

--- Assets/Scripts/GUI/SplashScreen.cs
+++ Assets/Scripts/GUI/SplashScreen.cs
@@ -1,17 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using GamepadInput;
+
 
 public class SplashScreen : MonoBehaviour
 {
     public string levelName = "MainMenu";
-    public float waitForSec = 3f;
+    public float waitForSec = 10f;
+
+    void Start() 
+    {
+        waitForSec = 7;
+    }
 	
 	void Update () 
     {
         waitForSec -= Time.deltaTime;
 
-        if (waitForSec < 0)
+        if (waitForSec < 0 || GamePad.GetButtonDown(GamePad.Button.A, GamePad.Index.Any))
             SceneManager.LoadScene(levelName);
 	}
 }
