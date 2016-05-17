8ced3fd10f5abf23ba6613521abe9c776d0a9713
Sam <sayid.malekdoost@gmail.com>
2016-05-17 16:26:16 +0200

    shit fixed

--- Assets/Scripts/PipeManager.cs
+++ Assets/Scripts/PipeManager.cs
@@ -2,261 +2,252 @@
 using System.Collections;
 using System.Collections.Generic;
 
-[RequireComponent(typeof(MapManager))]
-public class PipeManager : MonoBehaviour {
-    //ConveyorPipe Textures
-    [SerializeField]
-    private Material voidPipeMat;
+//[RequireComponent(typeof(MapManager))]
+public class PipeManager : MonoBehaviour
+{
+    ////ConveyorPipe Textures
+    //[SerializeField]
+    //private Material voidPipeMat;
 
-    [SerializeField]
-    private Material cornerPipeMat;
+    //[SerializeField]
+    //private Material cornerPipeMat;
 
-    [SerializeField]
-    private Material straightPipeMat;
+    //[SerializeField]
+    //private Material straightPipeMat;
 
-    [SerializeField]
-    private Material crossPipeMat;
+    //[SerializeField]
+    //private Material crossPipeMat;
 
-    [SerializeField]
-    private Material tPipeMat;
+    //[SerializeField]
+    //private Material tPipeMat;
 
-    [SerializeField]
-    private GameObject voidPipePrefab;
+    //[SerializeField]
+    //private GameObject voidPipePrefab;
 
-    [SerializeField]
-    private GameObject cornerPipePrefab;
+    //[SerializeField]
+    //private GameObject cornerPipePrefab;
 
-    [SerializeField]
-    private GameObject cornerPipePHPrefab;
+    //[SerializeField]
+    //private GameObject cornerPipePHPrefab;
 
-    [SerializeField]
-    private GameObject crossPipePrefab;
+    //[SerializeField]
+    //private GameObject crossPipePrefab;
 
-    [SerializeField]
-    private GameObject crossPipePHPrefab;
+    //[SerializeField]
+    //private GameObject crossPipePHPrefab;
 
-    [SerializeField]
-    private GameObject straightPipePrefab;
+    //[SerializeField]
+    //private GameObject straightPipePrefab;
 
-    [SerializeField]
-    private GameObject straightPipePHPrefab;
+    //[SerializeField]
+    //private GameObject straightPipePHPrefab;
 
-    [SerializeField]
-    private GameObject tPipePrefab;
+    //[SerializeField]
+    //private GameObject tPipePrefab;
 
-    [SerializeField]
-    private GameObject tPipePHPrefab;
+    //[SerializeField]
+    //private GameObject tPipePHPrefab;
 
-    private GameObject _boardReference;
+    //private GameObject _boardReference;
 
-    private MapManager _mapManager;
+    ////private MapManager _mapManager;
 
-    private List<Hashtable> _builtPipe;
-    
-    public Dictionary<PipeData.PipeType, Material> pipeTextures;
+    //private List<Hashtable> _builtPipe;
 
-    public GameObject pipeConnectionPrefab;
+    //public Dictionary<PipeData.PipeType, Material> pipeTextures;
 
-    private List<Hashtable> _placeHPipe;
-    void Awake()
-    {
-        _boardReference = GameObject.FindGameObjectWithTag("Board");
-        _mapManager = GetComponent<MapManager>();
+    //public GameObject pipeConnectionPrefab;
 
-        _builtPipe = new List<Hashtable>();
-        _placeHPipe = new List<Hashtable>();
+    //private List<Hashtable> _placeHPipe;
+    //void Awake()
+    //{
+    //    _boardReference = GameObject.FindGameObjectWithTag("Board");
+    //    //_mapManager = GetComponent<MapManager>();
 
-        for (int i = 0; i < _mapManager.nPlayers; i++) {
-            _builtPipe.Add(new Hashtable());
-            _placeHPipe.Add(new Hashtable());
-        }
-        
-        pipeTextures = new Dictionary<PipeData.PipeType, Material>()
-        {
-            {PipeData.PipeType.Void, voidPipeMat},
-            {PipeData.PipeType.Corner, cornerPipeMat},
-            {PipeData.PipeType.Cross, crossPipeMat},
-            {PipeData.PipeType.T, tPipeMat},
-            {PipeData.PipeType.Straight, straightPipeMat}
-        };
-    }
-<<<<<<< HEAD
-    //This function place a tiles of type at the position x,y with a rotation of rotation
-    public void placePipeOfTypeAt(int playerIndex,PipeType t, int x, int y,Quaternion rotation)
-=======
+    //    _builtPipe = new List<Hashtable>();
+    //    _placeHPipe = new List<Hashtable>();
 
-    public void placePipeOfTypeAt(int playerIndex,PipeData.PipeType t, int x, int y,Quaternion rotation)
->>>>>>> Development
-    {
+    //    //for (int i = 0; i < _mapManager.nPlayers; i++) {
+    //    //    _builtPipe.Add(new Hashtable());
+    //    //    _placeHPipe.Add(new Hashtable());
+    //    //}
 
-        if (!_builtPipe[playerIndex].ContainsKey(new Vector2(x, y)))
-        {
-           
-            Vector3 position;
-            GameObject g = new GameObject();
-            position = _mapManager.getTileByCoord(x, y).transform.position;
-            position.y += 1f;
-            switch (t)
-            {
-                case PipeData.PipeType.Corner:
-                    g = (GameObject)Instantiate(cornerPipePrefab, position, rotation);
-                    break;
-                case PipeData.PipeType.Straight:
-                    g = (GameObject)Instantiate(straightPipePrefab, position, rotation);
-                    break;
-                case PipeData.PipeType.Cross:
-                    g = (GameObject)Instantiate(crossPipePrefab, position, rotation);
-                    break;
-                case PipeData.PipeType.T:
-                    g = (GameObject)Instantiate(tPipePrefab, position, rotation);
-                    break;
-            }
-            g.name = "pipe" + (x + 1) + "_" + (y + 1);
-            g.transform.parent = _boardReference.transform;
-            PipeT pipe = new PipeT();
-            pipe.pipeType = t;
-            pipe.objRef = g;
-            pipe.playerIndex = playerIndex;
+    //    pipeTextures = new Dictionary<PipeData.PipeType, Material>()
+    //    {
+    //        {PipeData.PipeType.Void, voidPipeMat},
+    //        {PipeData.PipeType.Corner, cornerPipeMat},
+    //        {PipeData.PipeType.Cross, crossPipeMat},
+    //        {PipeData.PipeType.T, tPipeMat},
+    //        {PipeData.PipeType.Straight, straightPipeMat}
+    //    };
+    //}
+    //public void placePipeOfTypeAt(int playerIndex,PipeData.PipeType t, int x, int y,Quaternion rotation)
+    //{
 
-            _builtPipe[playerIndex].Add(new Vector2(x, y), pipe);
-        }
-    }
-<<<<<<< HEAD
-    //This function place a placeholder of type at the position x,y with a rotation of rotation
-    public void placePipePHOfTypeAt(int playerIndex, PipeType t, int x, int y)
-=======
+    //    if (!_builtPipe[playerIndex].ContainsKey(new Vector2(x, y)))
+    //    {
 
-    public void placePipePHOfTypeAt(int playerIndex, PipeData.PipeType t, int x, int y)
->>>>>>> Development
-    {
-        if (_placeHPipe[playerIndex].ContainsKey(new Vector2(x, y))){
-            foreach (Vector2 key in _placeHPipe[playerIndex].Keys)
-                GameObject.Destroy(((PipeT)_placeHPipe[playerIndex][key]).objRef);
-            _placeHPipe[playerIndex] = new Hashtable();
-        }
-            Vector3 position;
-            GameObject g = new GameObject();
-            position = _mapManager.getTileByCoord(x, y).transform.position;
-            position.y += 3f;
-            switch (t)
-            {
-                case PipeData.PipeType.Corner:
-                    g = (GameObject)Instantiate(cornerPipePHPrefab, position, Quaternion.identity);
-                    break;
-                case PipeData.PipeType.Straight:
-                    g = (GameObject)Instantiate(straightPipePHPrefab, position, Quaternion.identity);
-                    break;
-                case PipeData.PipeType.Cross:
-                    g = (GameObject)Instantiate(crossPipePHPrefab, position, Quaternion.identity);
-                    break;
-                case PipeData.PipeType.T:
-                    g = (GameObject)Instantiate(tPipePHPrefab, position, Quaternion.identity);
-                    break;
-            }
-            g.name = "pipePH" + (x + 1) + "_" + (y + 1);
-            g.GetComponent<tileManager>().xPos = x;
-            g.GetComponent<tileManager>().yPos = y;
-            g.GetComponent<tileManager>().updatePipeHolePositions();
-            g.transform.parent = _boardReference.transform;
-            PipeT pipe = new PipeT();
-            pipe.pipeType = t;
-            pipe.objRef = g;
-            pipe.playerIndex = playerIndex;
+    //        //Vector3 position;
+    //        GameObject g = new GameObject();
+    //       // position = _mapManager.getTileByCoord(x, y).transform.position;
+    //        //position.y += 1f;
+    //        //switch (t)
+    //        //{
+    //        //    case PipeData.PipeType.Corner:
+    //        //        g = (GameObject)Instantiate(cornerPipePrefab, position, rotation);
+    //        //        break;
+    //        //    case PipeData.PipeType.Straight:
+    //        //        g = (GameObject)Instantiate(straightPipePrefab, position, rotation);
+    //        //        break;
+    //        //    case PipeData.PipeType.Cross:
+    //        //        g = (GameObject)Instantiate(crossPipePrefab, position, rotation);
+    //        //        break;
+    //        //    case PipeData.PipeType.T:
+    //        //        g = (GameObject)Instantiate(tPipePrefab, position, rotation);
+    //        //        break;
+    //        //}
+    ////        g.name = "pipe" + (x + 1) + "_" + (y + 1);
+    ////        g.transform.parent = _boardReference.transform;
+    ////        PipeT pipe = new PipeT();
+    ////        pipe.pipeType = t;
+    ////        pipe.objRef = g;
+    ////        pipe.playerIndex = playerIndex;
 
-        _placeHPipe[playerIndex].Add(new Vector2(x, y), pipe);
-        
-    }
-    //Inner class used to store data relative to each placed pipe
-    public class PipeT {
-        public PipeData.PipeType pipeType;
-        public GameObject objRef;
-        public int playerIndex;
+    ////        _builtPipe[playerIndex].Add(new Vector2(x, y), pipe);
+    ////    }
+    ////}
 
-        private ArrayList possiblePositions;
+    ////public void placePipePHOfTypeAt(int playerIndex, PipeData.PipeType t, int x, int y)
+    ////{
+    ////    if (_placeHPipe[playerIndex].ContainsKey(new Vector2(x, y))){
+    ////        foreach (Vector2 key in _placeHPipe[playerIndex].Keys)
+    ////            GameObject.Destroy(((PipeT)_placeHPipe[playerIndex][key]).objRef);
+    ////        _placeHPipe[playerIndex] = new Hashtable();
+    ////    }
+    ////        //Vector3 position;
+    ////        GameObject g = new GameObject();
+    ////        //position = _mapManager.getTileByCoord(x, y).transform.position;
+    ////        //position.y += 3f;
+    ////        //switch (t)
+    ////        //{
+    ////        //    case PipeData.PipeType.Corner:
+    ////        //        g = (GameObject)Instantiate(cornerPipePHPrefab, position, Quaternion.identity);
+    ////        //        break;
+    ////        //    case PipeData.PipeType.Straight:
+    ////        //        g = (GameObject)Instantiate(straightPipePHPrefab, position, Quaternion.identity);
+    ////        //        break;
+    ////        //    case PipeData.PipeType.Cross:
+    ////        //        g = (GameObject)Instantiate(crossPipePHPrefab, position, Quaternion.identity);
+    ////        //        break;
+    ////        //    case PipeData.PipeType.T:
+    ////        //        g = (GameObject)Instantiate(tPipePHPrefab, position, Quaternion.identity);
+    ////        //        break;
+    ////        //}
+    //////        g.name = "pipePH" + (x + 1) + "_" + (y + 1);
+    //////        g.GetComponent<tileManager>().xPos = x;
+    //////        g.GetComponent<tileManager>().yPos = y;
+    //////        g.GetComponent<tileManager>().updatePipeHolePositions();
+    //////        g.transform.parent = _boardReference.transform;
+    //////        PipeT pipe = new PipeT();
+    //////        pipe.pipeType = t;
+    //////        pipe.objRef = g;
+    //////        pipe.playerIndex = playerIndex;
 
-        public void calculatePossiblePositions(int x, int y)
-        {
-            switch (pipeType)
-            {
-                case PipeData.PipeType.Corner:
-                    break;
-                case PipeData.PipeType.Cross:
-                    break;
-                case PipeData.PipeType.Straight:
-                    break;
-                case PipeData.PipeType.T:
-                    break;
-                
-            }
-        }
-    }
+    //////    _placeHPipe[playerIndex].Add(new Vector2(x, y), pipe);
 
-    public GameObject getCrossPipePrefab()
-    {
-        return crossPipePrefab;
-    }
+    //////}
+    ////////Inner class used to store data relative to each placed pipe
+    //////public class PipeT {
+    //////    public PipeData.PipeType pipeType;
+    //////    public GameObject objRef;
+    //////    public int playerIndex;
 
-    public GameObject getVoidPipePrefab()
-    {
-        return voidPipePrefab;
-    }
+    //////    private ArrayList possiblePositions;
 
-    public GameObject getTPipePrefab()
-    {
-        return tPipePrefab;
-    }
+    //////    public void calculatePossiblePositions(int x, int y)
+    //////    {
+    //////        switch (pipeType)
+    //////        {
+    //////            case PipeData.PipeType.Corner:
+    //////                break;
+    //////            case PipeData.PipeType.Cross:
+    //////                break;
+    //////            case PipeData.PipeType.Straight:
+    //////                break;
+    //////            case PipeData.PipeType.T:
+    //////                break;
 
-    public GameObject getCornerPipePrefab()
-    {
-        return cornerPipePrefab;
-    }
+    //////        }
+    //////    }
+    //////}
 
-    public GameObject getStraightPipePrefab()
-    {
-        return straightPipePrefab;
-    }
-    public GameObject getCrossPipePHPrefab()
-    {
-        return crossPipePHPrefab;
-    }
+    //////public GameObject getCrossPipePrefab()
+    //////{
+    //////    return crossPipePrefab;
+    //////}
 
-    public GameObject getTPipePHPrefab()
-    {
-        return tPipePHPrefab;
-    }
+    //////public GameObject getVoidPipePrefab()
+    //////{
+    //////    return voidPipePrefab;
+    //////}
 
-    public GameObject getCornerPipePHPrefab()
-    {
-        return cornerPipePHPrefab;
-    }
+    //////public GameObject getTPipePrefab()
+    //////{
+    //////    return tPipePrefab;
+    //////}
 
-    public GameObject getStraightPipePHPrefab()
-    {
-        return straightPipePHPrefab;
-    }
+    //////public GameObject getCornerPipePrefab()
+    //////{
+    //////    return cornerPipePrefab;
+    //////}
 
-    public void flushPHPipes(int playerIndex)
-    {
-        _placeHPipe[playerIndex] = new Hashtable();
-    }
+    //////public GameObject getStraightPipePrefab()
+    //////{
+    //////    return straightPipePrefab;
+    //////}
+    //////public GameObject getCrossPipePHPrefab()
+    //////{
+    //////    return crossPipePHPrefab;
+    //////}
 
-    public Hashtable getPipeOfPlayer(int index)
-    {
-        return _builtPipe[index];
-    }
+    //////public GameObject getTPipePHPrefab()
+    //////{
+    //////    return tPipePHPrefab;
+    //////}
 
-    public Hashtable getPHPipeOfPlayer(int index)
-    {
-        return _placeHPipe[index];
-    }
-    public void removePipeFromPlayer(int index,Vector2 key)
-    {
-        _builtPipe[index].Remove(key);
-    }
-    public void removePipePHFromPlayer(int index, Vector2 key)
-    {
-        _placeHPipe[index].Remove(key);
-    }
+    //////public GameObject getCornerPipePHPrefab()
+    //////{
+    //////    return cornerPipePHPrefab;
+    //////}
+
+    //////public GameObject getStraightPipePHPrefab()
+    //////{
+    //////    return straightPipePHPrefab;
+    //////}
+
+    //////public void flushPHPipes(int playerIndex)
+    //////{
+    //////    _placeHPipe[playerIndex] = new Hashtable();
+    //////}
+
+    //////public Hashtable getPipeOfPlayer(int index)
+    //////{
+    //////    return _builtPipe[index];
+    //////}
+
+    //////public Hashtable getPHPipeOfPlayer(int index)
+    //////{
+    //////    return _placeHPipe[index];
+    //////}
+    //////public void removePipeFromPlayer(int index,Vector2 key)
+    //////{
+    //////    _builtPipe[index].Remove(key);
+    //////}
+    //////public void removePipePHFromPlayer(int index, Vector2 key)
+    //////{
+    //////    _placeHPipe[index].Remove(key);
+    //////}
+
+
+    //    }
 }
-
-
